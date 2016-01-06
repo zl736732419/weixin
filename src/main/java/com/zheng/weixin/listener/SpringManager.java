0831aac06bf8084f04bfcac78c371590b642ab3b
zhenglian <736732419@qq.com>
2016-01-06 21:54:07 +0800

    修改springManager的配置方式，由spring管理,不需要在web.xml中配置

--- src/main/java/com/zheng/weixin/listener/SpringManager.java
+++ src/main/java/com/zheng/weixin/listener/SpringManager.java
@@ -3,6 +3,7 @@ package com.zheng.weixin.listener;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
 
 /**
  * 获取spring容器
@@ -10,6 +11,7 @@ import org.springframework.context.event.ContextRefreshedEvent;
  * @author zhenglian
  * @time 2015年12月25日 下午2:14:10
  */
+@Component
 public class SpringManager implements
 		ApplicationListener<ContextRefreshedEvent> {
 
